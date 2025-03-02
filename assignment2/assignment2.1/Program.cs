2056625470324d3c6cec68916a7867cc75ead3ed
小鲸鱼 <153498773+xiaojingyuba@users.noreply.github.com>
2025-03-02 22:34:28 +0800

    Add files via upload

---
+++ assignment2/assignment2.1/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+namespace assignment
+{
+    class DispPrime
+    {
+        static List<int> getprime(int a)
+        {
+            List<int> primelist = new List<int>();//用列表来存储素数因子    
+
+            int i = 2;//记录因子
+            int b = a;//记录被i整除一次前的a
+            while(i<=b)
+            {
+                //当i能整除a，则b记录被i整除一次前的a，a除以i
+                if (a % i == 0) 
+                { 
+                    b = a; 
+                    a /= i; 
+                }
+                //i不再能整除a时
+                else if (b % i == 0)
+                {
+                    primelist.Add(i); //将i添加到列表中，并且不会重复
+                    i++; 
+                    b = a; 
+                }
+                else i++; 
+            }
+            return primelist;
+        }
+        static void Main()
+        {
+            //读入输入的数字
+            Console.Write("请输入一个大于2的整数数据：");
+            string strnum = Console.ReadLine();
+
+            //判断输入数据是否为整数
+            int intnum;
+            bool Isint= int.TryParse(strnum, out intnum);//是整数则传给intnum
+
+            //输入的数字有误
+            if (!Isint || intnum < 2)
+                {
+                    Console.WriteLine("输入数据有误！");
+                    return;
+                }
+
+            List<int> primelist = getprime(intnum);//计算素数因子
+            Console.Write("{0}的所有素数因子为：", intnum);
+            Console.WriteLine(string.Join(" ", primelist));//打印素数因子
+        }
+    }
+}
---
+++ assignment2/assignment2.2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace assignment2
+{
+    class ArrOpr
+    {
+        static void Main()
+        {
+            Console.WriteLine("请输入整数数组的数据，以空格分隔：");
+            string input = Console.ReadLine();
+            // 将输入字符串按空格分割成字符数组
+            string[] strarr = input.Split(new[] { ' ' });
+            //若标志位为假即输入有误，就不调用相关计算函数
+            bool flag = true;
+
+            int length = strarr.Length;
+            int[] intarr = new int[length];
+            for (int i = 0;i<length;i++)
+            {
+                //将每个字符转为整型，判断是否为整数
+                if (!int.TryParse(strarr[i], out intarr[i]))
+                {
+                    Console.WriteLine("输入的{0}不是整数", strarr[i]);
+                    flag = false;
+                }
+            }
+            if (flag == true)
+            {
+                //计算最大值
+                int max = GetMaxNum(intarr);
+                Console.WriteLine("该整数数组的最大值为：" + max);
+                //计算最小值
+                int min = GetMinNum(intarr);
+                Console.WriteLine("最小值为：" + min);
+                //计算平均值
+                double average = GetArg(intarr);
+                Console.WriteLine("平均值为：" + average);
+                //计算平均值
+                int sum = GetSum(intarr);
+                Console.WriteLine("所以数组元素的和为：" + sum);
+            }
+        }
+
+        //计算最大值
+        static int GetMaxNum(int[] arr)
+        {
+            int max = arr[0];
+            for(int i = 0;i<arr.Length;i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+            return max;
+        }
+
+        //计算最小值
+        static int GetMinNum(int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                    min = arr[i];
+            }
+            return min;
+        }
+
+        //计算平均值
+        static double GetArg(int[] arr)
+        {
+            int length = arr.Length;
+            int sum = 0;
+            for (int i = 0; i < length; i++)
+                sum+= arr[i];
+            double average = (double)sum / length;
+            return average;
+        }
+
+        //计算数据总和
+        static int GetSum(int[] arr)
+        {
+            int length = arr.Length;
+            int sum = 0;
+            for (int i = 0; i < length; i++)
+                sum += arr[i];
+            return sum; 
+        }
+    }
+}
