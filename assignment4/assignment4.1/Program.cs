b7242c05167142a955cfa3333974418f23c558a9
xiaojingyuba <2506455664@qq.com>
2025-03-19 18:37:49 +0800

    add assignment4

---
+++ assignment4/assignment4.1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+namespace assignment4
+{
+    public class Node<T>//定义结点类型
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)//构造函数
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>//泛型链表类
+    {
+        public Node<T> head;
+        public Node<T> tail;
+
+        //构造函数，头结点尾结点都指向空
+        public GenericList()
+        {
+            head = tail = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        //向链表中加入元素
+        public void Add(T t)
+        {
+            //以新加的元素新建结点
+            Node<T> n = new Node<T>(t);
+
+            //如果链表为空，头结点尾结点都指向新结点
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            //不为空，则尾结点指向新结点，并更新尾结点
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        //传入一个委托作为参数
+        public void Foreach(Action<T> action)
+        {
+            Node<T> p = head;
+
+            //对链表中的每个元素进行操作
+            while (p != null)
+            {
+                action(p.Data);
+                p = p.Next;
+            }
+        }       
+    }
+
+    class Listforeach
+    {
+        static void Main(string[] args)
+        {
+            //新建链表并加入元素
+            GenericList<int> intlist = new GenericList<int>();
+            intlist.Add(10);
+            intlist.Add(7);
+            intlist.Add(15);
+            intlist.Add(23);
+            intlist.Add(2);
+
+            //输出每个元素
+            Console.WriteLine("该列表的元素有：");
+            intlist.Foreach(x => Console.WriteLine(x));
+
+            //求出最大值
+            int max = intlist.head.Data;
+            intlist.Foreach(x => { if (x > max) max = x; });
+            Console.WriteLine("最大值为："+max);
+
+            //求出最小值
+            int min = intlist.head.Data;
+            intlist.Foreach(x => { if (x < min) min = x; });
+            Console.WriteLine("最小值为："+min);
+
+            //求出元素总和
+            int sum = 0;
+            intlist.Foreach(x => sum += x);
+            Console.WriteLine("总和为："+sum);
+        }
+    }
+}
+    
+   
---
+++ assignment4/assignment4.2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+
+public class AlarmClock
+{
+    //定义Tick事件
+    public event EventHandler Tick;
+    //定义Alarm事件
+    public event EventHandler Alarm;
+
+    //闹钟的定时时间
+    private int alarmtime;
+
+    //设置闹钟时间
+    public AlarmClock(int time)
+    {
+        alarmtime = time;
+    }
+
+    //启动闹钟
+    public void Start()
+    {
+        int i;
+        for (i = 0; i < alarmtime; i++)
+        {
+            //模拟每秒走时
+            Thread.Sleep(1000); 
+            //每秒嘀嗒一下
+            OnTick();
+        }
+        //到达定时时间则响铃
+        if (i == alarmtime)
+            OnAlarm();
+
+    }
+
+    //嘀嗒事件
+    protected virtual void OnTick()
+    {
+        if (Tick != null)
+            Tick(this, EventArgs.Empty);
+    }
+
+    //响铃事件
+    protected virtual void OnAlarm()
+    {
+        if (Alarm != null)
+            Alarm(this, EventArgs.Empty);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //实例化闹钟对象，并设置定时时间
+        AlarmClock clock = new AlarmClock(15);
+
+        //为嘀嗒事件添加一个处理方法
+        void OnTick(object sender, EventArgs e)
+        {
+            Console.WriteLine("Tick...");
+        }
+        clock.Tick += OnTick;
+
+        //为响铃事件添加一个处理方法
+        void OnAlarm(object sender, EventArgs e)
+        {
+            Console.WriteLine("Alarm!");
+        }
+        clock.Alarm += OnAlarm;
+
+        //启动闹钟
+        clock.Start();
+    }
+}
